8d17c7e1d5751e39435c2fad607920c92b8d8520
Bandana-K <bandana.kumari81@gmail.com>
2025-01-24 20:02:04 +0530

    Time: 2 ms (99.1%), Space: 43.2 MB (68.85%)

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        boolean result = true;
+        var upperCaseString = s.toUpperCase();
+
+        for(int i = 0, j = s.length()-1; i <= j; ) {
+            var leftChar = upperCaseString.charAt(i);
+            var rightChar = upperCaseString.charAt(j);
+
+            if(leftChar == rightChar){
+                i++;
+                j--;
+            } else if(!isValidAlphaNumeric(leftChar)){
+                i++;
+            } else if(!isValidAlphaNumeric(rightChar)){
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return result;
+    }
+
+    boolean isValidAlphaNumeric(char check){
+        return ((check >= '0' && check <= '9') || (check >= 'A' && check <= 'Z'));
+    }
+}
