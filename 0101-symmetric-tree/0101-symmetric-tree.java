6fa738435f1428517b95ff5e0b058fa5d1dca737
Bandana-K <bandana.kumari81@gmail.com>
2025-01-28 10:26:34 +0530

    Time: 0 ms (100%), Space: 42.2 MB (12.29%)

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        else 
+            return areSymmetric(root.left, root.right);
+        
+    }
+
+    public boolean areSymmetric(TreeNode left, TreeNode right) {
+        if(left == null && right == null)
+            return true;
+        if((left == null && right != null) 
+             || (left != null && right == null)
+                || (left.val != right.val))
+            return false;            
+        else {
+            return areSymmetric(left.left, right.right) && areSymmetric(left.right, right.left);
+        }      
+    }
+}
