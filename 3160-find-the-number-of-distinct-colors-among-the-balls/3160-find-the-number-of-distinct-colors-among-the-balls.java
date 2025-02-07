4892bc8f33484f3e8d7c90206c735f831bd0d1a9
Bandana-K <bandana.kumari81@gmail.com>
2025-02-07 16:52:20 +0530

    Time: 50 ms (15.72%), Space: 97.5 MB (46.29%)

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
         int[] ans = new int[queries.length];
-        Map<Integer, Set<Integer>> colorMap = new HashMap<>();
+        Map<Integer, Integer> colorMap = new HashMap<>();  // We don't really need to know the ball numbers where a color is applied but only the number of balls to which this color is applied. So instead of Set<Integer> we can do the same task in Integer aswell.
         Map<Integer, Integer> ballMap = new HashMap<>();
 
         for(int i = 0; i< queries.length; i++){
             int ball = queries[i][0], color = queries[i][1];
             if(ballMap.containsKey(ball)){
                 int previousColor = ballMap.get(ball);
-                var previousBalls = colorMap.get(previousColor);
-                previousBalls.remove(ball);
-                if(previousBalls.size() == 0)
+                colorMap.put(previousColor, colorMap.get(previousColor) -1 );
+                if(colorMap.get(previousColor) == 0)
                     colorMap.remove(previousColor);
             }
             ballMap.put(ball, color);
-            Set<Integer> balls = colorMap.containsKey(color) ? colorMap.get(color) : new HashSet<>();
-            balls.add(ball);
-            colorMap.put(color, balls);
+            colorMap.put(color, colorMap.getOrDefault(color, 0)+1);
+
             ans[i] = colorMap.keySet().size();
         }
-
         return ans;
     }
 }
