da14c03111ec553582703e41313590ede4866086
Bandana-K <bandana.kumari81@gmail.com>
2025-02-08 14:07:12 +0530

    Time: 12 ms (9.45%), Space: 48.3 MB (80%)

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        int n1 = countLength(headA);
+        int n2 = countLength(headB);
+
+        System.out.println("Length of A = " + n1);
+        System.out.println("Length of B = " + n2);
+
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+
+        if(n1 > n2){ //List1 is big, so move the temp1 pointer ahead by lengthDiff
+            int lengthDiff = n1 - n2;
+            for(int i = 1; i <= lengthDiff; i++){
+                System.out.println("current i = " + i + " current temp1 = " + temp1.val);
+                temp1 = temp1.next;
+            }
+        } else if(n1 < n2){ //List2 is big, so move the temp2 pointer ahead by lengthDiff
+            int lengthDiff = n2 - n1;
+            for(int i = 1; i <= lengthDiff; i++){
+                System.out.println("current i = " + i + " current temp2 = " + temp2.val);
+                temp2 = temp2.next;
+            }
+        }
+        
+        while(temp1 != null && temp1 != temp2){
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        if(temp1 == null) return null;
+        return temp1;
+            
+    }
+
+    private int countLength(ListNode head){
+        if (head == null)
+            return 0;
+
+        return 1+ countLength(head.next);    
+    }
+}
