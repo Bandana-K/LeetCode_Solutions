3bdfe4de729ca47c0eaab0be1070c61822338527
Bandana-K <bandana.kumari81@gmail.com>
2025-02-08 10:58:57 +0530

    Time: 1 ms (100%), Space: 44.4 MB (82.04%)

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,11 +10,30 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
         
-                ListNode temp = new ListNode();
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+    
+        int carry = 0;
+        while(l1 != null || l2 != null || carry  != 0){
+            int sum = 0;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            } 
+            if (l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
 
-        ListNode newHead = new ListNode();
-        newHead.next = temp;
+            sum += carry;
+            ListNode sumNode = new ListNode(sum %10);
+            carry = sum/10;
+
+            temp.next = sumNode;
+            temp = temp.next;
+        }
+
+        return dummy.next;
     }
 }
