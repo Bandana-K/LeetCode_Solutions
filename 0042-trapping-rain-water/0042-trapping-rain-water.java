97acf583c3facb5d66628f4c60a753e85f6ca74b
Bandana-K <bandana.kumari81@gmail.com>
2025-02-09 13:06:50 +0530

    Time: 1 ms (64.69%), Space: 45.6 MB (97.8%)

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,30 @@
+// Water stored at any unit is = Min(leftMaxHeightBuilding, rightMaxHeightBuilding) - height of current unit
+class Solution {
+    public int trap(int[] height) {
+
+        int n = height.length;
+
+        int[] leftMax = new int[n];
+        int currentLeftMax = height[0];
+        leftMax[0] = currentLeftMax;
+        for(int i = 1; i < n; i++){
+            currentLeftMax = Math.max(currentLeftMax, height[i]);
+            leftMax[i] = currentLeftMax;
+        }
+
+        int[] rightMax = new int[n];
+        int currentRightMax = height[n-1];
+        rightMax[n-1] = currentRightMax;
+        for(int i = n-2; i >= 0 ; i--){
+            currentRightMax = Math.max(currentRightMax, height[i]);
+            rightMax[i] = currentRightMax;
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans += (Math.min(leftMax[i], rightMax[i]) - height[i]);
+        }
+        
+        return ans;
+    }
+}
