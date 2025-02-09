51d23ffdd0e7c37b743bec686efd234872dd32f1
Bandana-K <bandana.kumari81@gmail.com>
2025-02-09 14:31:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.99%)

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,10 +2,7 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int nonVal = 0;
         for(int i = 0; i< nums.length; i++){
-            if(nums[i] != val){
-                nonVal++;
-                nums[nonVal-1] = nums[i];
-            }
+            if(nums[i] != val) nums[nonVal++] = nums[i];
         }
         return nonVal;
     }
