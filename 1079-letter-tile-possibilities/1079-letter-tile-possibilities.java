8077458c11888d6fb15af4ed8a4e3bd879a3d554
Bandana-K <bandana.kumari81@gmail.com>
2025-02-18 05:25:46 +0530

    Time: 0 ms (100%), Space: 41.7 MB (74.04%)

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -3,6 +3,7 @@ class Solution {
         char[] arr = tiles.toCharArray();
         return permute(0, arr);
     }
+    
     public int permute(int start, char[] arr) {
         if(start == arr.length) return 0;
         int ans = 0;
@@ -15,38 +16,16 @@ class Solution {
         }
         return ans;
     }
+
     public boolean isPermutedBefore(int i, int j, char ch, char[] arr) {
         while(i<=j)
             if(arr[i++] == ch) return true;
         return false;
     }
+
     public void swap(int i, int j, char[] arr) {
         char temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 }
-
-
-// class Solution {
-//     public int numTilePossibilities(String tiles) {
-//         var charArray = tiles.toCharArray();
-//         Arrays.sort(charArray);
-//         Set<String> result = new HashSet<>();
-//         var currentSubSeq = new StringBuilder();
-//         findSubsequence(0, charArray, result, currentSubSeq);
-//         System.out.print(result);
-//         return result.size();
-//     }
-
-//     void findSubsequence(int idx, char[] charArray, Set<String> result, StringBuilder currentSubSeq){
-//         result.add(currentSubSeq.toString());
-//         for(int i = 0; i < charArray.length; i++){
-//             // if(i != idx && charArray[i] == charArray[i-1]) continue;
-//             if(i == idx) continue;
-//             currentSubSeq.append(charArray[i]);
-//             findSubsequence(i+1, charArray, result, currentSubSeq);
-//             currentSubSeq.deleteCharAt(currentSubSeq.length() - 1);
-//         }
-//     }
-// }
