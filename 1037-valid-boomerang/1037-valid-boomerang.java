39aaf8c5c8e9ff1ce41e11cdceb8ef0f4fdf0063
Bandana-K <bandana.kumari81@gmail.com>
2025-02-18 09:26:03 +0530

    Time: 12 ms (1%), Space: 42.1 MB (7.82%)

--- 1037-valid-boomerang/1037-valid-boomerang.java
+++ 1037-valid-boomerang/1037-valid-boomerang.java
@@ -5,14 +5,14 @@ class Solution {
         if(points[1][0] == points[2][0] && points[1][1] == points[2][1]) return false;  // check if point2 and point3 are same return false
         if(points[0][0] == points[2][0] && points[0][1] == points[2][1]) return false;  // check if point1 and point3 are same return false
 
-        // Given the 3 points, to check if they are not in same line, we can calculate the slope for any 2 set of points.
+        // Given the 3 points, to check if they are not in same line, we can calculate the slope by taking any 2 set of points.
         // If the slope is same all 3 are in same lines, else not. But calculating slope might give error of divide by 0.
 
-        //check for x-axis
         double slope1 = Math.abs(1.0 * (points[1][1] - points[0][1])/(points[1][0] - points[0][0]));
         double slope2 = Math.abs(1.0 * (points[2][1] - points[1][1])/(points[2][0] - points[1][0]));
+        double slope3 = Math.abs(1.0 * (points[0][1] - points[2][1])/(points[0][0] - points[2][0]));
 
-        System.out.println("slope1 = " + slope1 + " slope2 = " + slope2);
-        return slope1 != slope2;
+        System.out.println("slope1 = " + slope1 + " slope2 = " + slope2 + " slope3 = " + slope3);
+        return (slope1 != slope2) || (slope2 != slope3);
     }
 }
