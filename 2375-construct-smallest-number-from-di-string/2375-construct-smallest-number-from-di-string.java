c512b749e4fdabe0944fb8f1c5affb8c5acd0e34
Bandana-K <bandana.kumari81@gmail.com>
2025-02-18 21:14:40 +0530

    Time: 1 ms (75.25%), Space: 41.6 MB (76.89%)

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String smallestNumber(String pattern) {
-        var visited = new boolean[100];
+        var visited = new boolean[10];
 
         StringBuilder currentNum = new StringBuilder();
         var possible = canFormNumber(pattern, 0, visited, currentNum);
@@ -11,12 +11,12 @@ class Solution {
     private boolean canFormNumber(String pattern, int idx, boolean[] visited, StringBuilder currentNum){
         if(idx > pattern.length()) return true;
 
-        for(char i = '1'; i <= '9' ; i++){
+        for(int i = 1; i <= 9 ; i++){
 
             if(visited[i]) continue;
 
             if(idx == 0){
-                currentNum.append(i);
+                currentNum.append((char) ('0' + i));
                 visited[i] = true;
                 var possible = canFormNumber(pattern, idx+1, visited, currentNum);
                 if(possible) return true;
@@ -25,10 +25,10 @@ class Solution {
             } else {
                 char direction = pattern.charAt(idx-1);
                 char prevChar = currentNum.charAt(currentNum.length()-1);
-                if(direction == 'I' && i <= prevChar) continue; 
-                if(direction == 'D' && i >= prevChar) continue; 
+                if(direction == 'I' && (char) ('0' + i) <= prevChar) continue; 
+                if(direction == 'D' && (char) ('0' + i) >= prevChar) continue; 
 
-                currentNum.append(i);
+                currentNum.append((char) ('0' + i));
                 visited[i] = true;
                 var possible = canFormNumber(pattern, idx+1, visited, currentNum);
                 if(possible) return true;
