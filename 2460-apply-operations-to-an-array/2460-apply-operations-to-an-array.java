9e5982de4c5d032d0e5772f95b56c931da9f2cd0
Bandana-K <bandana.kumari81@gmail.com>
2025-03-01 12:45:33 +0530

    Time: 1 ms (96.37%), Space: 44.3 MB (17.11%)

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for(int i = 0; i < n-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] = nums[i]*2;
+                nums[i+1] = 0;
+            }
+        }
+
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0){
+                result[j++] = nums[i];
+            }
+        }
+
+        for(; j < n; j++){
+            result[j] = 0;
+        }
+
+        return result;
+    }
+}
