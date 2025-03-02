b376a5a66dedb9e8bb20ea0ee5d4f64b869b3666
Bandana-K <bandana.kumari81@gmail.com>
2025-03-02 19:41:46 +0530

    Time: 4 ms (68.29%), Space: 56.3 MB (86.86%)

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,15 +1,28 @@
+//Idea is to calculate maxPositiveSum and minNegativeSum for the entire array.
+//The final array will be the max of (maxPositiveSum,  abs(minNegativeSum))
 class Solution {
     public int maxAbsoluteSum(int[] nums) {
-        int result = 0;
-        for(int i = 0; i< nums.length; i++){
-            int currSum = nums[i];
-            result = Math.max(result, Math.abs(currSum));
-            for(int j = i+1; j< nums.length; j++){
-                currSum = currSum + nums[j];
-                result = Math.max(result, Math.abs(currSum));
 
-            }
+        int maxPosSum = nums[0], currPosSum = nums[0];
+        for(int i = 1; i< nums.length; i++){
+            if(currPosSum + nums[i] < nums[i])
+                currPosSum = nums[i];
+            else 
+                currPosSum += nums[i];
+            maxPosSum = Math.max(maxPosSum, currPosSum);
         }
-        return result;
+
+        int minNegSum = nums[0], currNegSum = nums[0];
+        for(int i = 1; i< nums.length; i++){
+            if(currNegSum + nums[i] > nums[i])
+                currNegSum = nums[i];
+            else 
+                currNegSum += nums[i];
+            minNegSum = Math.min(minNegSum, currNegSum);
+        }
+
+        // System.out.println("maxPosSum = " + maxPosSum + " minNegSum = " + minNegSum);
+        return Math.max(maxPosSum, minNegSum*-1);
+
     }
 }
