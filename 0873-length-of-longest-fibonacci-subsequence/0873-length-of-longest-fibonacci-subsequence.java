0e0030cce1331f5687b0bf3d7fba770f3be51cb0
Bandana-K <bandana.kumari81@gmail.com>
2025-03-02 21:17:50 +0530

    Time: 198 ms (21.21%), Space: 45 MB (81.45%)

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
@@ -1,27 +1,41 @@
-//COPIED
-
 class Solution {
     public int lenLongestFibSubseq(int[] arr) {
-        int n = arr.length, maxLen = 0;
-        var dp = new int[n][n];
-
-        for(var curr=2; curr<n; curr++){
-            int left = 0, right = curr - 1;
-            while(left < right){
-                var sum = arr[left] + arr[right];
-                if(sum == arr[curr]){
-                    dp[right][curr] = 1 + dp[left][right];
-                    maxLen = Math.max(maxLen, dp[right][curr]);
-                    right--;
-                    left++;
-                }
-                else if(sum < arr[curr])
-                    left++;
-                else
-                    right--;
-            }
+        int n = arr.length, result = 0;
+        Map<Long, Integer> arrIdx = new HashMap<>();
+        for(int i=0; i < n; i++){
+            arrIdx.put((long) arr[i], i);
         }
 
-        return maxLen > 0 ? maxLen + 2 : 0; // +2 because we did not count first 2 numbers
+        // System.out.println(arrIdx);
+
+        for(int i = 0; i < n ; i++){
+            int x1 = arr[i], count = 0, k=0;
+            // System.out.println(" i = "+i );
+            for(int j = i+1; j<n; ){
+                long sum = (long) x1 + arr[j];
+                // System.out.println(" x1 = " + x1 + " j = "+j + " arr[j] = " + arr[j]);
+                if(arrIdx.containsKey(sum)){
+                    if(count == 0){
+                        count = count+3;
+                        k = j;
+                    } else count++;
+                    x1 = arr[j];
+                    j = arrIdx.get(sum);
+                    // System.out.println(" sum = " + sum + " found, new j = " + j + " count = "+ count);
+                    result = Math.max(result, count);
+                } else {
+                    if(count == 0) j++;
+                    else{
+                         j = k+1;
+                         count = 0;
+                         x1 = arr[i];
+                    }
+                }
+            }
+            // System.out.println(" result = "+ result);
+
+        }
+
+        return result;
     }
 }
