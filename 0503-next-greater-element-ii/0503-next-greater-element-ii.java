4df1c52f978103a49903867e940a814672fc226a
Bandana-K <bandana.kumari81@gmail.com>
2025-03-08 14:25:31 +0530

    Time: 13 ms (78.09%), Space: 46 MB (81.75%)

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,47 +1,27 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
 
-        int[] newNum = new int[nums.length*2];
-        int[] newNumMax = new int[nums.length*2];
-        for(int i = 0; i < nums.length; i++){
-            newNum[i] = nums[i];
-            // System.out.print(newNum[i] + " ");
-        }
-
-        for(int i = 0; i < nums.length; i++){
-            newNum[nums.length+i] = nums[i];
-            // System.out.print(newNum[nums.length+i] + " ");
-        }
-
-        // System.out.println();
-
         Stack<Integer> stack = new Stack<>();
-        newNumMax[newNumMax.length-1] = -1;
-        stack.push(-1);
-        for(int i = newNum.length-2; i >= 0; i--){
+        int[] result = new int[nums.length];
+
+        for(int i = nums.length-1 ; i >= 0; i--)
+            stack.push(nums[i]);
+
+        for(int i = nums.length-1; i >= 0; i--){
+            int nextIdx = (i+1)%nums.length;
             int nextMax = -1;
-            // System.out.println("i = " + i + " newNum[i] = " + newNum[i]);
-            // System.out.println("Previous stack = " + stack);
-            if(newNum[i] < newNum[i+1]) nextMax = newNum[i+1];
+            if(nums[i] < nums[nextIdx]) nextMax = nums[nextIdx];
             else {
-                while(!stack.isEmpty() && stack.peek() <= newNum[i])
+                while(!stack.isEmpty() && stack.peek() <= nums[i])
                     stack.pop();
                 
                 if(stack.isEmpty()) {
-                    // System.out.println("Stack is empty");
                     stack.push(-1);
                 }
                 else nextMax = stack.peek();    
             }
-            // System.out.println("New stack = " + stack);
-            // System.out.println("NextMax = "  + nextMax);
-            newNumMax[i] = nextMax;
+            result[i] = nextMax;
             stack.push(nextMax);
-        }
-
-        int[] result = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            result[i] = newNumMax[i];
         }
 
         return result;
