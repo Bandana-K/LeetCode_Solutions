0129807d6943b699a2859a046894c2f138906996
Bandana-K <bandana.kumari81@gmail.com>
2025-03-08 20:50:26 +0530

    Time: 1 ms (92.1%), Space: 41.8 MB (18.39%)

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,12 +1,17 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
         int result = Integer.MAX_VALUE;
-        for(int i = 0 ; i+k <= blocks.length(); i++){
-            int white = 0;
-            for(int j = i; j < i+k; j++){
-                if(blocks.charAt(j) == 'W')
-                    white++;
-            }
+        int white = 0;
+        for(int i = 0 ; i < k; i++){
+            if(blocks.charAt(i) == 'W') white++;
+        }
+        if(white == 0) return 0;
+        result = Math.min(result, white);
+
+        for(int i = k; i<blocks.length(); i++){
+            int whiteAdded = blocks.charAt(i) == 'W' ? 1 : 0;
+            int whiteRemoved = blocks.charAt(i-k) == 'W' ? 1 : 0;
+            white = white + whiteAdded - whiteRemoved;
             if(white == 0) return 0;
             result = Math.min(result, white);
         }
