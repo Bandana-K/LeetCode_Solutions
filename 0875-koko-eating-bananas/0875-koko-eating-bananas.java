c22b6339e5f71afafa491dd6877baafd169dbd5e
Bandana-K <bandana.kumari81@gmail.com>
2025-03-15 17:20:48 +0530

    Time: 12 ms (75.62%), Space: 45.1 MB (75.7%)

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -10,31 +10,22 @@ class Solution {
         int result = 0;
         while(left <= right){
             int mid = (left + right)/2;
-            // System.out.println("left = " + left + " right = " + right + " mid = " + mid);
-
             if(canEat(mid, piles, h)){
                 result = mid;
-                // System.out.println(" result = " + result);
                 right = mid - 1;
             } else {
                 left = mid+1;
             }
         }
-
         return result;
     }
 
     boolean canEat(int limit, int[] piles, int hours){
         long hoursTaken = 0;
         for(int i=0; i<piles.length; i++){
-            if(piles[i] <= limit){
-                hoursTaken++;
-            } else{
-                hoursTaken += (long)piles[i] / limit;
-                if(piles[i] % limit != 0) hoursTaken++;
-            }
+            hoursTaken += (long)piles[i] / limit;
+            if(piles[i] % limit != 0) hoursTaken++;  
         }
-        // System.out.println("For limit = " + limit + " hoursTaken = " + hoursTaken);
         return hoursTaken <= hours;
     }
 }
