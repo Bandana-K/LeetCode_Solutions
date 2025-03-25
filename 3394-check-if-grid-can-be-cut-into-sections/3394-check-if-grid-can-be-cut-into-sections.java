a702ebad17c0f693ce2f1a8517cce13335201e0d
Bandana-K <bandana.kumari81@gmail.com>
2025-03-25 21:34:01 +0530

    Time: 99 ms (60.09%), Space: 117.7 MB (87.28%)

--- 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.java
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.java
@@ -3,25 +3,12 @@ class Solution {
         List<int[]> mergedX = merge(rectangles, 0);
         List<int[]> mergedY = merge(rectangles, 1);
 
-        // System.out.println("Printing mergedX " + mergedX);
-        // for(int[] xbox: mergedX){
-        //     System.out.print(xbox[0] + " " + xbox[1] + " ");
-        // }
-
-        // System.out.println("Printing mergedY " + mergedY);
-        // for(int[] xbox: mergedY){
-        //     System.out.print(xbox[0] + " " + xbox[1] + " ");
-        // }
-
         if(mergedX.size() >=3 || mergedY.size() >=3) return true;
         return false;
     }
 
     List<int[]> merge(int[][] rectangles, int idx){
         Arrays.sort(rectangles, ((a,b) -> a[idx]-b[idx]));
-        // for(int[] x: rectangles){
-        //     System.out.println(x[0] + " " + x[1] + " " + x[2] + " " + x[3]);
-        // }
         List<int[]> merged = new ArrayList<>();
         int start = rectangles[0][idx], end = rectangles[0][idx+2];
         for(int i = 1; i<rectangles.length; i++){
