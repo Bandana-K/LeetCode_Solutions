e75dbc80989e6c2a75bf48742dc1c97187a96055
Bandana-K <bandana.kumari81@gmail.com>
2025-03-30 09:35:06 +0530

    Time: 8 ms (49.76%), Space: 42.5 MB (35.57%)

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -3,29 +3,26 @@ class Solution {
         
         int n = s.length();
 
-        Map<Character, List<Integer>> charFreq = new HashMap<>();
+        Map<Character, Integer> charFreq = new HashMap<>();
         for(int i=0; i<n; i++){
-            charFreq.putIfAbsent(s.charAt(i), new ArrayList<>());
-            charFreq.get(s.charAt(i)).add(i);
+            if(!charFreq.containsKey(s.charAt(i)))
+                charFreq.put(s.charAt(i), i);
         }
 
-
-        List<Integer> rev = new ArrayList<>();
-        for(int i = n-1; i>=0;){
-            int idx = charFreq.get(s.charAt(i)).get(0);
-            int j = i-1;
-            while(j >= idx){
-                idx = Math.min(idx, charFreq.get(s.charAt(j)).get(0));
-                j--;
-            }
-            rev.add(i-j);
-            i = j;
-        }
 
         List<Integer> result = new ArrayList<>();
-        for(int i = rev.size()-1; i>=0; i--)
-            result.add(rev.get(i));
-            
+        for(int i = n-1; i>=0;){
+            int idx = charFreq.get(s.charAt(i));
+            int j = i-1;
+            while(j >= idx){
+                idx = Math.min(idx, charFreq.get(s.charAt(j)));
+                j--;
+            }
+            result.add(i-j);
+            i = j;
+        }
+        
+        Collections.reverse(result);
         return result;
     }
 }
