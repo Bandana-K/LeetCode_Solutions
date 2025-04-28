c032e4dfe91979e4be566e42867a0815c8ac2bb9
Bandana-K <bandana.kumari81@gmail.com>
2025-04-28 21:21:00 +0530

    Time: 24 ms (22.74%), Space: 59.8 MB (20.69%)

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -6,20 +6,8 @@ class Solution {
         for(int i = 0; i<nums.length; i++){
             if(idxMap.containsKey(nums[i]) && (i - idxMap.get(nums[i])) <= k)
                 return true;
-            else
-                idxMap.put(nums[i], i);
+            idxMap.put(nums[i], i);
         }
         return false;
     }
-
-
-    // public boolean containsNearbyDuplicate(int[] nums, int k) {
-    //     boolean result = false;
-    //     int n = nums.length;
-    //     for(int i = 0; i < (n-k); i++){
-    //         for(int l = i+1; l <= (i+k); l++)
-    //             if(nums[i] == nums[l]) return true;
-    //     }
-    //     return false;
-    // }
 }
